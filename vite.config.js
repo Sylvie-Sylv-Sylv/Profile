5cb3c9dcd372bb3742e38d11699af91a38e84d8c
SylvieSylvSylv <sylvie.sylv.sylv@gmail.com>
2026-08-11 22:53:33 +0700

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { mdsvex } from 'mdsvex';
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -16,14 +16,17 @@ export default defineConfig({
 			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter(),
+			adapter: adapter({
+				fallback: '404.html'
+			}),
 			preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
 			extensions: ['.svelte', '.svx', '.md'],
 			typescript: {
 				config: (config) => {
 					config.include.push('../drizzle.config.js');
 				}
-			}
+			},
+
 		})
 	]
 });
